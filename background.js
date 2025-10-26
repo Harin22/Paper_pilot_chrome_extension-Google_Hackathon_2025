8fe4589217f955b51532f36e21e4bf7547b9d068
Harin <84919488+Harin22@users.noreply.github.com>
2025-10-26 10:45:43 +0530

    Refactor background.js for clean code

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
-// background.js
 
-// 1. Listen for the user clicking the extension icon
 chrome.action.onClicked.addListener((tab) => {
-  console.log("ICON CLICKED, trying to open panel..."); // We added this line
+  console.log("ICON CLICKED, trying to open panel..."); // added for backup
   chrome.sidePanel.open({ tabId: tab.id });
+
 });
