03c71822d251f03623b09600925c0cff1d345001
Harin <84919488+Harin22@users.noreply.github.com>
2025-10-27 20:21:12 +0530

    Simplify content.js by removing comments
    Removed unnecessary comments about file usage.

--- content.js
+++ content.js
@@ -1,8 +1,3 @@
 // content.js - SIMPLIFIED VERSION
-// This file can now be minimal or even removed entirely
-// since PDF extraction happens in the side panel
-
-// You can leave this empty or remove the content_scripts 
-// section from manifest.json entirely
 
 console.log("Paper Pilot content script loaded");
